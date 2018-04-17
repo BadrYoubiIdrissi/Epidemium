11bb7ab916615d774d1a4cfb1acd021f82d06718
Badr Youbi Idrissi <badryoubiidrissi@gmail.com>
2018-04-17 18:33:37 +0200

    Initial structure

---
+++ DataPreparation/Database.py
@@ -0,0 +1,11 @@
+"""
+
+Author : Badr YOUBI IDRISSI
+
+This file will contain the Database object which loads the different dataframes needed and prepares them
+
+Different steps include : 
+    - 
+
+
+"""
