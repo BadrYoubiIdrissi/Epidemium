155a3c4d68ece19e86f6d026e826f86d398f4770
Badr Youbi Idrissi <badryoubiidrissi@gmail.com>
2018-05-08 21:35:03 +0200

    Cleaned code and added rolling window prediction

--- LSTM/NN.py
+++ LSTM/NN.py
@@ -5,18 +5,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 
-y = np.linspace(0,10*np.pi,1000)**2
-print(y.shape)
-# plt.plot(y, label="Training data")
+y = np.sin(np.linspace(0,20*np.pi,1000))
 
-def toSupervised(a, windowSize):
-    scaler = MinMaxScaler(feature_range=(-1,1))
+def to2D(a):
     if a.ndim == 1: #MinMaxScaler n'accepte que les tableaux de dimmension 2
         a = a.reshape(a.shape[0],1)
-    a = scaler.fit_transform(a) #On normalise
+
+def scale(a):
+    to2D(a)
+    scaler = MinMaxScaler(feature_range=(-1,1))
+    scaled = scaler.fit_transform(a) #On normalise
+    return scaler, scaled
+def toSupervised(a, windowSize):
+    to2D(a)
     orig = a.copy() #On garde une copie car on va modifier a
     a = a[windowSize:] #On enlève les occurences qui n'ont pas assez de valeurs dans le passé
-    
     sh = a.shape
     a = a.reshape((sh[0],1,sh[1])) #On reshape a pour être dans la forme exigée par Keras
     for i in range(windowSize):
@@ -25,16 +28,26 @@ def toSupervised(a, windowSize):
         b = b[windowSize:] #On donne à b la même forme que a
         b = b.reshape((sh[0],1,sh[1]))
         a = np.concatenate((b,a), axis=1) #On rajoute sur la deuxième dimmension les décalages temporels
-    return a, scaler
+    return a
 
 def inputOutput(a):
     return a[:,:-1,:], a[:,-1,:]
 
+def threeDimInput(a):
+    if a.ndim == 2:
+        return a.reshape((1, a.shape[0], a.shape[1]))
+    else:
+        return a.reshape((1, a.shape[0], 1))
+
 windowSize = 40
 nbFeatures = 1
 neurons = 50
-data, scaler = toSupervised(y, windowSize)
-train, test = data[:800], data[800:]
+train_raw, test_raw = y[:800], y[800:]
+scaler, train_scaled = scale(train_raw)
+test_scaled = scaler.transform(test_raw)
+train = toSupervised(train_scaled, windowSize)
+test = toSupervised(test_scaled, windowSize)
+
 
 # for i in range(train.shape[1]):
 #     plt.plot(test[:,i,:])
@@ -44,7 +57,7 @@ model = Sequential()
 model.add(CuDNNLSTM(neurons, input_shape = (windowSize, nbFeatures), return_sequences=True))
 model.add(Dropout(0.5))
 model.add(CuDNNLSTM(256))
-model.add(Dense(1))
+model.add(Dense(nbFeatures))
 model.add(Activation("linear"))
 model.compile(loss="mse", optimizer="adam")
 model.summary()
@@ -54,6 +67,16 @@ test_in , test_out = inputOutput(test)
 
 model.fit(x=train_in,y=train_out,epochs=100, shuffle=False)
 
-plt.plot(model.predict(train_in))
+rollingWindow = train_in[-1,:,:]
+nPred = 1000
+pred = []
+for i in range(nPred):
+    p = model.predict(threeDimInput(rollingWindow))
+    pred.append(p[0,0])
+    rollingWindow = np.concatenate((rollingWindow, p), axis=0)[1:]
+plt.plot(pred); plt.plot(test_out)
+
+
+
 plt.plot(train_out)
 plt.show()
